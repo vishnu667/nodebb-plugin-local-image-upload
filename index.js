f32a74b7627fefee948c6e6f2c57c19d3ea66a81
smartameer <smartameer@icloud.com>
2017-02-07 15:57:07 +0530

    Fixing files upload to a userid based directory

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 (function(module) {
+
     'use strict';
     var winston = module.parent.require('winston'),
-        async = module.parent.require('async'),
-        file = module.parent.require('./file'),
-        plugin = {};
+    async = module.parent.require('async'),
+    file = module.parent.require('./file'),
+    user = module.parent.require('./user'),
+    fs = require('fs'),
+    path = require('path'),
+    nconf = require('nconf');
+
+    var plugin = {};
 
     plugin.init = function(params, callback) {
         callback();
     };
 
     plugin.upload = function (data, callback) {
-        var settings,
-            image = data.image;
-
+        var settings;
+        var image = data.image;
+        var uid = data.uid;
+        var folder = 'files';
         async.waterfall([
             function (next) {
-                winston.log('image :' + image.path);
-                var fileName = new Date().getTime() + image.name;
-                file.saveFileToLocal(fileName, 'files', image.path, function (err,data) {
+                winston.log('image '+image.path);
+                var fileName = new Date().getTime()+image.name;
+                var uploadPath = path.join(nconf.get('base_dir'), nconf.get('upload_path'), folder, ''+uid);
+                if (!fs.existsSync(uploadPath)) {
+                    fs.mkdirSync(uploadPath);
+                } else {
+                    winston.log('Folder exists');
+                }
+
+                file.saveFileToLocal(fileName, 'files/'+uid, image.path, function(err, data){
                     if (err) {
                         winston.error(err);
                     } else {
@@ -31,6 +45,5 @@
             }
         ], callback);
     };
-
     module.exports=plugin;
 }(module));
